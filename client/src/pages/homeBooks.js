511cbc3bb6505b76a2eaae26e9e5b05b07e97752
Dwaraka Krishna <36559963+dwaraka-vutla@users.noreply.github.com>
2023-11-21 16:50:31 -0600

    Update homeBooks.js

--- client/src/pages/homeBooks.js
+++ client/src/pages/homeBooks.js
@@ -23,35 +23,43 @@ const homeBooks ={
         },
         {
             title:'Non-Fiction',
-            details:"Satiate your thirst for knowledge with our non-fiction selection. This genre encompasses a wide range of topics, including history, science, self-help, biographies, and more, offering insights and wisdom for inquisitive minds."        
+            details:"Satiate your thirst for knowledge with our non-fiction selection. This genre encompasses a wide range of topics, including history, science, self-help, biographies, and more, offering insights and wisdom for inquisitive minds." ,       
+            img: 'https://assets.gqindia.com/photos/63ad3f8f02491fecd2d16d70/16:9/w_1920,c_limit/Best-Indian-Non-Fiction_01.jpg'       
         },
         {
             title:'History and Biography',
-            details:"Travel through time and immerse yourself in the lives of historical figures with our history and biography books. Gain a deeper understanding of past events and the extraordinary people who have shaped our world."
+            details:"Travel through time and immerse yourself in the lives of historical figures with our history and biography books. Gain a deeper understanding of past events and the extraordinary people who have shaped our world.",
+            img: 'https://www.bookgeeks.in/wp-content/uploads/2020/06/Best-Indian-Books-by-Indian-Authors-1.jpg'
         },
         {
             title:'Self-Help and Personal Development',
-            details:"Invest in your personal growth and well-being with our self-help and personal development books. Discover strategies for success, happiness, and self-improvement to live your best life."
+            details:"Invest in your personal growth and well-being with our self-help and personal development books. Discover strategies for success, happiness, and self-improvement to live your best life.",
+            img: 'https://assets.gqindia.com/photos/62e0ff519abb0d9c2ce1567c/16:9/w_1920,c_limit/best-self-help-books-01.jpg'        
         },
         {
             title:'Cookbooks',
-            details:"Unleash your inner chef and explore the culinary world with our cookbooks. Whether you're a novice or a seasoned cook, our collection features a smorgasbord of delectable recipes to tantalize your taste buds."
+            details:"Unleash your inner chef and explore the culinary world with our cookbooks. Whether you're a novice or a seasoned cook, our collection features a smorgasbord of delectable recipes to tantalize your taste buds.",
+            img: 'https://assets.vogue.in/photos/5e53a3866dd25800087efa28/2:3/w_1920,c_limit/cookbooks1.jpg'
         },
         {
             title:"Children's and Young Adult",
-            details:"Foster a love for reading in the younger generation with our children's and young adult books. From charming picture books to engrossing young adult novels, there's a story waiting to ignite young imaginations."
+            details:"Foster a love for reading in the younger generation with our children's and young adult books. From charming picture books to engrossing young adult novels, there's a story waiting to ignite young imaginations.",
+            img: 'https://i0.wp.com/indiacurrents.com/wp-content/uploads/2021/12/TOP-10-OF-2021-1-1.jpg?w=785&ssl=1'
         },
         {
             title:"Science and Nature",
-            details:"For the curious minds eager to understand the world around them, our science and nature books offer a window into the wonders of the natural world and the latest scientific discoveries."
+            details:"For the curious minds eager to understand the world around them, our science and nature books offer a window into the wonders of the natural world and the latest scientific discoveries.",
+            img: 'https://m.media-amazon.com/images/I/A1177z+SGEL._SY466_.jpg'
         },
         {
             title:"Travel",
-            details:"Plan your next adventure or embark on a virtual journey with our travel guides and travel narratives. Explore the beauty and diversity of our world from the comfort of your reading nook."
+            details:"Plan your next adventure or embark on a virtual journey with our travel guides and travel narratives. Explore the beauty and diversity of our world from the comfort of your reading nook.",
+            img: 'http://ileadstreetlibrary.com/wp-content/uploads/2019/04/5-Best-Travel-Guide-Books-for-India.jpg'        
         },
         {
             title:"Art and Photography",
-            details:"Immerse yourself in the visual arts with our art and photography books. Admire the creativity of artists and photographers from around the world, and let their work inspire your own creativity."
+            details:"Immerse yourself in the visual arts with our art and photography books. Admire the creativity of artists and photographers from around the world, and let their work inspire your own creativity.",
+            img: 'https://m.media-amazon.com/images/I/41WsMutjIXL._SX342_SY445_.jpg'       
         }
     ],
     endContent:"At our online bookstore, you're invited to peruse these genres and more, as you embark on a literary adventure of your choosing. We're dedicated to providing a wide range of titles and genres to ensure that every reader finds something to cherish. Happy reading!"
